302338aee20d568e63e65ddcf64d8b42ecc47f6f
Musfique Ahmed <mahmed2330101@bsds.uiu.ac.bd>
2026-01-20 21:11:43 +0600

    Add generator example for twin primes
    Introduced a new section on generators in final.ipynb, including code that generates and prints twin prime pairs. This expands the notebook's coverage of Python generator concepts with a practical example.

--- he.py
+++ he.py
@@ -24,24 +24,45 @@ print(p1, p2, p3)'''''
 
 
 
-import copy
-class A:
-    def __init__(self,a,b):
-        self.a=a
-        self.b=b
-    def copy(self):
-        return A(self.a,self.b)
-    def __str__(self):
-        return f"({self.a},{self.b})"
-    def multiply(self,other):
-        self.a=self.a* other
-        self.b=self.b // other
-s1 = A(21,31)
-s2 = s1
-s3 = s2.copy()
-s4= copy.deepcopy(s3)
-s1.multiply(2)
-s2.multiply(2)
-s3.multiply(2)
-s4.multiply(2)
-print(s1,s2,s3,s4)
+# import copy
+# class A:
+#     def __init__(self,a,b):
+#         self.a=a
+#         self.b=b
+#     def copy(self):
+#         return A(self.a,self.b)
+#     def __str__(self):
+#         return f"({self.a},{self.b})"
+#     def multiply(self,other):
+#         self.a=self.a* other
+#         self.b=self.b // other
+# s1 = A(21,31)
+# s2 = s1
+# s3 = s2.copy()
+# s4= copy.deepcopy(s3)
+# s1.multiply(2)
+# s2.multiply(2)
+# s3.multiply(2)
+# s4.multiply(2)
+# print(s1,s2,s3,s4)
+
+def is_prime(number):
+   if number <= 1:
+       return False
+   for i in range(2, number):
+       if number % i == 0:
+           return False
+   return True
+def prime_printer(limit):
+   prev = 2
+   for i in range(3, limit):
+       if is_prime(i):
+           if i-prev == 2:
+               pair = (prev , i)
+               yield pair
+           prev = i
+
+
+a = prime_printer(1000)
+for i in a:
+   print(i)
